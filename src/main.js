13a361a54a78f2eb61fb2148d5474681b30660f3
Thiébaud Modoux <tmodoux@pryv.com>
2019-12-19 14:14:43 +0100

    Clean vuetify defaults

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import Vuetify from 'vuetify';
+import Vuetify from 'vuetify/lib';
 import 'vuetify/dist/vuetify.min.css';
-import vuetify from './plugins/vuetify';
-
-Vue.config.productionTip = false;
 
 Vue.use(Vuetify);
+const vuetify = new Vuetify({});
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
